97acec5fe929fd5299cfd7cdd5e23d2e34dcd887
jznf <jan@zavesky.info>
2018-03-04 21:39:18 +0100

    0.6363586450024741 /// 9.470535683998605 /// 0.08480850599880796

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,29 @@
 import timeit
+import uuid
 
 
-def test():
+def add_stuff():
     x = 1
     for i in range(10**7):
         x = i + x
 
 
+def write_stuff():
+    with open('stuff.txt', '+w') as stuff:
+        stuff_to_write = ''
+        for i in range(10**6):
+            stuff_to_write += str(uuid.uuid4())
+        stuff.write(stuff_to_write)
+
+
+def read_stuff():
+    with open('stuff.txt', 'r') as stuff:
+        read_stuff = stuff.readline()
+        print(read_stuff[:20])
+        print(read_stuff[-20:])
+
+
 if __name__ == '__main__':
-    print(timeit.timeit("test()", setup="from __main__ import test", number=1))
+    print(timeit.timeit("add_stuff()", setup="from __main__ import add_stuff", number=1))
+    print(timeit.timeit("write_stuff()", setup="from __main__ import write_stuff", number=1))
+    print(timeit.timeit("read_stuff()", setup="from __main__ import read_stuff", number=1))
